d3f3363ae41b8ae9d4219b180cc72bcd65e10caa
Sen Wang <moli.wang@gmail.com>
2013-07-08 16:18:26 +0800

    add code to control mouse(use Win32 API)

--- PerC_UsingCPP/Main.cpp
+++ PerC_UsingCPP/Main.cpp
@@ -1,4 +1,5 @@
 ﻿#include<iostream>
+#include<Windows.h>
 #include "util_pipeline.h"
 using namespace std ;
 
@@ -17,6 +18,19 @@ class MyPipeline: public UtilPipeline
 		virtual void PXCAPI OnRecognized(PXCVoiceRecognition::Recognition *data) 
 		{
 			wprintf_s(L"You say : <%s>\n",data->dictation);
+			if( data->dictation[0] == 'm')
+				Move(50) ;
+			else if ( data->dictation[0] == 'p')
+				Move(-50) ;
+		}
+		void Move(int stepX)
+		{
+			cout << "移動滑鼠 : " << stepX << endl ;
+			POINT p;
+			if (GetCursorPos(&p))
+				cout << "X:" << p.x << "  ,Y:" << p.y << endl ;
+			
+			SetCursorPos(p.x+stepX,p.y) ;
 		}
 };
 
